23ed55e09932dad48fddca542b998eda912f88ed
Terwilly <sylvainterwilly@gmail.com>
2022-09-15 11:08:00 +0100

    cart added

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const db = require('./data/db.json');
 const products = require('./data/products.json');
 const signupUsers = require('./data/signupUsers.json');
+const cart = require('./data/cart.json');
 
 module.exports = {
   db,
   products,
   signupUsers,
+  cart,
 };
